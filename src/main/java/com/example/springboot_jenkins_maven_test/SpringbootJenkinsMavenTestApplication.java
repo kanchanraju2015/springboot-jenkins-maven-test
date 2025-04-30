d98dcd0ac9d46da84d16040210a477f1bfe5f94c
kanchan raju <kanchanraju2015@gmail.com>
2025-04-30 14:56:39 +0530

    first maven with jenkins commit

---
+++ src/main/java/com/example/springboot_jenkins_maven_test/SpringbootJenkinsMavenTestApplication.java
@@ -0,0 +1,28 @@
+package com.example.springboot_jenkins_maven_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootJenkinsMavenTestApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootJenkinsMavenTestApplication.class, args);
+	}
+	@RequestMapping("/jenkins")
+	public String test()
+	{
+		return "this is jenkins test";
+	}
+	@RequestMapping("/maven")
+	public String test1()
+	{
+		return "this is maven test";
+	}
+	
+
+}
---
+++ src/test/java/com/example/springboot_jenkins_maven_test/SpringbootJenkinsMavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jenkins_maven_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsMavenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
