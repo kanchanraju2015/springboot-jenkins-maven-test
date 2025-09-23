684594514d90c50b743508995572302ee8e8566d
kanchan raju <kanchanraju2015@gmail.com>
2025-09-23 13:46:25 +0530

    2nd commit

--- src/main/java/com/example/springboot_jenkins_maven_test/SpringbootJenkinsMavenTestApplication.java
+++ src/main/java/com/example/springboot_jenkins_maven_test/SpringbootJenkinsMavenTestApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+// create simple maven project 
+// add git project and repo url 
+// into trigger snapshot no maven target no pre steps
+// it will automatically pull pom.xml file into goals clean install only
+// all default no maven pre built steps
 
 @RestController
 @SpringBootApplication
@@ -28,6 +33,5 @@ public class SpringbootJenkinsMavenTestApplication {
 	{
 		return "this is branch test";
 	}
-	
 
 }
